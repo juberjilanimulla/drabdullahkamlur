b81609371866b96f93369dcbf6796e6c47567ea9
Juber_Mulla <juberjilanimulla@gmail.com>
2025-10-14 11:21:53 +0530

    conatact changes

--- routes/admin/admincontactRouter.js
+++ routes/admin/admincontactRouter.js
@@ -5,30 +5,59 @@ import contactmodel from "../../model/contactmodel.js";
 const admincontactRouter = Router();
 
 admincontactRouter.post("/", getallcontactHandler);
-admincontactRouter.post("/create", createcontactHandler);
-admincontactRouter.put("/update", updatecontactHandler);
 admincontactRouter.delete("/delete", deletecontactHandler);
 
 export default admincontactRouter;
 
-async function getallcontactHandler(params) {
+async function getallcontactHandler(req, res) {
   try {
-  } catch (error) {
-    console.log("error", error);
-    errorResponse(res, 500, "internal server error");
-  }
-}
+    const { pageno = 0, filterBy = {}, sortby = {}, search = "" } = req.body;
+    const limit = 10;
+    const skip = pageno * limit;
 
-async function createcontactHandler(req, res) {
-  try {
-  } catch (error) {
-    console.log("error", error);
-    errorResponse(res, 500, "internal server error");
-  }
-}
+    let query = {};
 
-async function updatecontactHandler(req, res) {
-  try {
+    // Apply search
+    if (search.trim()) {
+      const searchRegex = new RegExp(search.trim(), "i");
+      query.$or = [
+        { name: { $regex: searchRegex } },
+        { email: { $regex: searchRegex } },
+        { subject: { $regex: searchRegex } },
+      ];
+    }
+
+    // Apply filters
+    if (filterBy && Object.keys(filterBy).length > 0) {
+      query = {
+        ...query,
+        ...filterBy,
+      };
+    }
+
+    // Sorting logic
+    const sortBy =
+      Object.keys(sortby).length !== 0
+        ? Object.keys(sortby).reduce((acc, key) => {
+            acc[key] = sortby[key] === "asc" ? 1 : -1;
+            return acc;
+          }, {})
+        : { createdAt: -1 };
+
+    // Fetch paginated blogs
+    const contact = await contactmodel
+      .find(query)
+      .sort(sortBy)
+      .skip(skip)
+      .limit(limit);
+
+    const totalCount = await contactmodel.countDocuments(query);
+    const totalPages = Math.ceil(totalCount / limit);
+
+    successResponse(res, "successfully", {
+      contact,
+      totalPages,
+    });
   } catch (error) {
     console.log("error", error);
     errorResponse(res, 500, "internal server error");
@@ -37,6 +66,14 @@ async function updatecontactHandler(req, res) {
 
 async function deletecontactHandler(req, res) {
   try {
+    const { _id } = req.body;
+    if (!_id) {
+      return errorResponse(res, 400, "some params are missing");
+    }
+    const contact = await contactmodel.findById({ _id: _id });
+    if (!contact) {
+      return errorResponse(res, 404, "contact details not found");
+    }
   } catch (error) {
     console.log("error", error);
     errorResponse(res, 500, "internal server error");
