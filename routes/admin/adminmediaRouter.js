621d28ea8093387d41f76530e3e705d0fe842a88
Juber_Mulla <juberjilanimulla@gmail.com>
2025-10-15 14:55:56 +0530

    this commit for admin media

--- routes/admin/adminmediaRouter.js
+++ routes/admin/adminmediaRouter.js
@@ -2,18 +2,78 @@ import { Router } from "express";
 import mediamodel from "../../model/mediamodel.js";
 import { successResponse, errorResponse } from "../../helper/serverResponse.js";
 import adminmediaimageRouter from "./adminuploadmediaimageRouter.js";
+import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 
 const adminmediaRotuer = Router();
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 adminmediaRotuer.post("/", getallmediaHandler);
 adminmediaRotuer.post("/create", createmediaHandler);
 adminmediaRotuer.put("/update", updatemediaHandler);
 adminmediaRotuer.delete("/delete", deletemediaHandler);
-adminmediaimageRouter.use("/upload", adminmediaimageRouter);
+adminmediaRotuer.use("/upload", adminmediaimageRouter);
+adminmediaRotuer.delete("/deleteimage", getdeleteimageHandler);
+
 export default adminmediaRotuer;
 
 async function getallmediaHandler(req, res) {
   try {
+    const { pageno = 0, filterBy = {}, sortby = {}, search = "" } = req.body;
+    const limit = 10;
+    const skip = pageno * limit;
+
+    let query = {};
+
+    // Apply search
+    if (search.trim()) {
+      const searchRegex = new RegExp(search.trim(), "i");
+      query.$or = [
+        { title: { $regex: searchRegex } },
+        { metadescription: { $regex: searchRegex } },
+        { keywords: { $regex: searchRegex } },
+        { content: { $regex: searchRegex } },
+        { category: { $regex: searchRegex } },
+      ];
+    }
+
+    // Apply filters
+    if (filterBy && Object.keys(filterBy).length > 0) {
+      query = {
+        ...query,
+        ...filterBy,
+      };
+    }
+
+    // Sorting logic
+    const sortBy =
+      Object.keys(sortby).length !== 0
+        ? Object.keys(sortby).reduce((acc, key) => {
+            acc[key] = sortby[key] === "asc" ? 1 : -1;
+            return acc;
+          }, {})
+        : { createdAt: -1 };
+
+    // Fetch paginated medias
+    const media = await mediamodel
+      .find(query)
+      .sort(sortBy)
+      .skip(skip)
+      .limit(limit);
+
+    const totalCount = await mediamodel.countDocuments(query);
+    const totalPages = Math.ceil(totalCount / limit);
+
+    successResponse(res, "successfully", {
+      media,
+      totalPages,
+    });
   } catch (error) {
     console.log("error", error);
     errorResponse(res, 500, "internal server error");
@@ -62,28 +122,56 @@ async function updatemediaHandler(req, res) {
 
 async function deletemediaHandler(req, res) {
   try {
-    // const { _id } = req.body;
-    // if (!_id) {
-    //   return errorResponse(res, 400, "blog ID (_id) is required");
-    // }
-    // // Find property before deletion (to access images)
-    // const blog = await blogmodel.findById(_id);
-    // if (!blog) {
-    //   return errorResponse(res, 404, "blog not found");
-    // }
-    // // Delete all images from S3
-    // const s3Key = blog.coverimage?.split(".amazonaws.com/")[1];
-    // if (s3Key) {
-    //   await s3.send(
-    //     new DeleteObjectCommand({
-    //       Bucket: process.env.AWS_BUCKET_NAME,
-    //       Key: s3Key,
-    //     })
-    //   );
-    // }
-    // // Delete blog from DB
-    // await blogmodel.findByIdAndDelete(_id);
-    // return successResponse(res, "blog and associated images deleted");
+    const { _id } = req.body;
+    if (!_id) {
+      return errorResponse(res, 400, "media ID (_id) is required");
+    }
+    // Find property before deletion (to access images)
+    const media = await mediamodel.findById(_id);
+    if (!media) {
+      return errorResponse(res, 404, "media not found");
+    }
+    // Delete all images from S3
+    const s3Key = media.image?.split(".amazonaws.com/")[1];
+    if (s3Key) {
+      await s3.send(
+        new DeleteObjectCommand({
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Key: s3Key,
+        })
+      );
+    }
+    // Delete media from DB
+    await mediamodel.findByIdAndDelete(_id);
+    return successResponse(res, "media and associated images deleted");
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
+
+async function getdeleteimageHandler(req, res) {
+  try {
+    const { _id } = req.body;
+    if (!_id) {
+      return errorResponse(res, 400, "Media ID (_id) is required");
+    }
+    const media = await mediamodel.findById(_id);
+    if (!media) {
+      return errorResponse(res, 404, "media not found");
+    }
+    const imageUrl = media.image;
+    const s3Key = imageUrl?.split(".amazonaws.com/")[1];
+    if (s3Key) {
+      const deleteCommand = new DeleteObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: s3Key,
+      });
+      await s3.send(deleteCommand);
+    }
+    media.image = ""; // Clear image reference from DB
+    await media.save();
+    return successResponse(res, "media image deleted successfully", media);
   } catch (error) {
     console.log("error", error);
     errorResponse(res, 500, "internal server error");
