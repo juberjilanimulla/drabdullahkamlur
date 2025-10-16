0f438102a36b526bd09bcdc41ad646e2f3d395e3
Juber_Mulla <juberjilanimulla@gmail.com>
2025-10-16 16:31:18 +0530

    this blog readers counts

--- routes/users/userblogRouter.js
+++ routes/users/userblogRouter.js
@@ -5,6 +5,8 @@ import blogmodel from "../../model/blogmodel.js";
 const userblogRouter = Router();
 
 userblogRouter.get("/", getallblogHandler);
+userblogRouter.get("/:id", getsingleblogHandler);
+userblogRouter.get("/readers/:id", getreadercountHandler);
 
 export default userblogRouter;
 
@@ -18,5 +20,45 @@ async function getallblogHandler(req, res) {
   } catch (error) {
     console.log("error", error);
     errorResponse(res, 500, "internal server error");
+  }
+}
+
+async function getsingleblogHandler(req, res) {
+  try {
+    const { id } = req.params;
+    let blog = await blogmodel.findById(id);
+
+    if (!blog) {
+      return errorResponse(res, 404, "Blog not found");
+    }
+
+    const previousReaders = blog.readers;
+    blog.readers += 1;
+    await blog.save();
+
+    const currentReaders = blog.readers;
+    successResponse(res, "Blog fetched successfully", {
+      blog,
+    });
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "Internal server error");
+  }
+}
+
+async function getreadercountHandler(req, res) {
+  try {
+    const { id } = req.params;
+    const blog = await blogmodel.findById(id);
+
+    if (!blog) {
+      return errorResponse(res, 404, "Blog not found");
+    }
+    successResponse(res, "Reader count fetched successfully", {
+      readers: blog.readers,
+    });
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "Internal server error");
   }
 }
